a4a05291714477b5edb41518ce95b3e71c48cb35
Magnus Evensberget <magnus.evensberget@nav.no>
2021-06-09 12:41:53 +0200

    Koblet til brukernotifikasjon

--- src/main/kotlin/no/nav/fo/veilarbvarsel/config/kafka/utils/props/KafkaAvroProducerProperties.kt
+++ src/main/kotlin/no/nav/fo/veilarbvarsel/config/kafka/utils/props/KafkaAvroProducerProperties.kt
@@ -1,7 +1,8 @@
 package no.nav.fo.veilarbvarsel.config.kafka.utils.props
 
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroSerializer
+import no.nav.common.kafka.util.KafkaPropertiesPreset.onPremDefaultProducerProperties
+import no.nav.common.utils.NaisUtils
 import no.nav.fo.veilarbvarsel.config.KafkaEnvironment
 import org.apache.kafka.clients.producer.ProducerConfig
 import java.util.*
@@ -9,12 +10,13 @@ import java.util.*
 data class KafkaAvroProducerProperties(val env: KafkaEnvironment) {
 
     fun getProperties(): Properties {
-        val properties = Properties()
+        val credentials = NaisUtils.getCredentials("service_user")
+        val properties = onPremDefaultProducerProperties("VEILARBVARSEL", env.bootstrapServers, credentials)
 
-        properties[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = env.bootstrapServers
+        //properties[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = env.bootstrapServers
         properties[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
         properties[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
-        properties[AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG] = env.schemaRegistryUrl
+        //properties[AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG] = env.schemaRegistryUrl
 
         return properties
     }
