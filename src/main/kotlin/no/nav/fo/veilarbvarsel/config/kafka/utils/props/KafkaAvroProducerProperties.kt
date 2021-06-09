e3072df328d5a89c2bb05ad2a93e456c7450a808
Magnus Evensberget <magnus.evensberget@nav.no>
2021-06-09 12:44:27 +0200

    Koblet til brukernotifikasjon

--- src/main/kotlin/no/nav/fo/veilarbvarsel/config/kafka/utils/props/KafkaAvroProducerProperties.kt
+++ src/main/kotlin/no/nav/fo/veilarbvarsel/config/kafka/utils/props/KafkaAvroProducerProperties.kt
@@ -1,5 +1,6 @@
 package no.nav.fo.veilarbvarsel.config.kafka.utils.props
 
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroSerializer
 import no.nav.common.kafka.util.KafkaPropertiesPreset.onPremDefaultProducerProperties
 import no.nav.common.utils.NaisUtils
@@ -16,7 +17,7 @@ data class KafkaAvroProducerProperties(val env: KafkaEnvironment) {
         //properties[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = env.bootstrapServers
         properties[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
         properties[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
-        //properties[AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG] = env.schemaRegistryUrl
+        properties[AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG] = env.schemaRegistryUrl
 
         return properties
     }
