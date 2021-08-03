6411dbb72f5e6246189e271af23ac2df85c89fb2
Magnus Evensberget <magnus.evensberget@nav.no>
2021-08-03 17:08:47 +0200

    Cleanup

--- application/src/main/kotlin/no/nav/fo/veilarbvarsel/application/configuration/KafkaConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.fo.veilarbvarsel.application.configuration
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.annotation.EnableKafka
-
-@EnableKafka
-@Configuration
-class KafkaConfiguration {
-}
