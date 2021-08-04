600fb7dba212a3b3d9ca6f2f3df2fc4f3b003901
Magnus Evensberget <magnus.evensberget@nav.no>
2021-08-04 08:34:45 +0200

    Used wrong annotation...

--- application/src/main/kotlin/no/nav/fo/veilarbvarsel/application/controllers/InternalController.kt
+++ application/src/main/kotlin/no/nav/fo/veilarbvarsel/application/controllers/InternalController.kt
@@ -1,9 +1,9 @@
 package no.nav.fo.veilarbvarsel.application.controllers
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class InternalController {
 
     @GetMapping("/isReady")
