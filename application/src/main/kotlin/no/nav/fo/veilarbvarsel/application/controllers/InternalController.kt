f7a8e1de93e1636aa71538d75378a046c09a2f18
Magnus Evensberget <magnus.evensberget@nav.no>
2021-08-04 08:44:12 +0200

    Used wrong annotation...

--- application/src/main/kotlin/no/nav/fo/veilarbvarsel/application/controllers/InternalController.kt
+++ application/src/main/kotlin/no/nav/fo/veilarbvarsel/application/controllers/InternalController.kt
@@ -1,17 +1,16 @@
 package no.nav.fo.veilarbvarsel.application.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/internal")
 class InternalController {
 
     @GetMapping("/isReady")
-    fun isReady() {
-    }
+    fun isReady(){}
 
     @GetMapping("/isAlive")
-    fun isAlive() {
-    }
-
+    fun isAlive(){}
 }
